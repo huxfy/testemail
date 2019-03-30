1d1a07a836c58a92721f2e0ce2fe1e556bbeaa52
fengyuan.hx <fengyuan.hx@alibaba-inc.com>
2019-03-30 21:52:37 +0800

    test

--- app/middleware/imglog.js
+++ app/middleware/imglog.js
@@ -4,7 +4,7 @@ const debug = require('debug')('test-email:test');
 module.exports = options => {
   return async function imglog(ctx, next) {
 
-    debug('请求来了');
+    debug(`请求来了，访问链接 ${ctx.url}`);
     debug('-------------------------headers----------------');
     debug('%O', ctx.request.headers);
     debug('-------------------------ip----------------');
